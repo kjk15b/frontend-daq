318d201fac1d00add0896b61866ecfc2da7ff006
Kolby Kiesling <kjk15b@acu.edu>
2022-02-19 11:02:17 -0600

    updated ingest routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, request
 import sys
 
 dataStream = {'UR' : list(),
@@ -8,13 +8,15 @@ dataStream = {'UR' : list(),
 
 app = Flask(__name__)
 
-@app.route('/data/ingest/<sensor>/<value>')
-def ingestRoute(sensor, value):
-    value = int(value)
-    print("received: {}, value={}".format(sensor, value))
-    if len(dataStream[sensor]) > 5:
-        dataStream[sensor].pop(0)
-    dataStream[sensor].append(value)
+@app.route('/data/ingest/<sensor>', methods=['POST'])
+def ingestRoute(sensor):
+    if request.method == "POST":
+        data = request.form.to_dict()
+        value = data['data']
+        print("received: {}, value={}".format(sensor, value))
+        if len(dataStream[sensor]) > 5:
+            dataStream[sensor].pop(0)
+        dataStream[sensor].append(value)
 
 if __name__ == '__main__':
     app.run(debug=sys.argv[1], host=sys.argv[2],
